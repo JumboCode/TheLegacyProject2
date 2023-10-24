afeee93646ebb7dea9bca326a0e4b5d24edd77e6
JuliaZel <julia.zelevinsky@gmail.com>
2023-10-24 18:42:25 -0400

    Updated styling of SideBar with new design

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,56 +1,116 @@
 import Link from "next/link";
 import Image from "next/legacy/image";
 import { signOut } from "next-auth/react";
+import {
+  faHouse,
+  faUsers,
+  faUserGroup,
+  faUserPlus,
+  faCircleUser,
+  faCity,
+  faArrowRightFromBracket,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SidebarItem = ({ label } : { label: string }) => {
+interface Button {
+  name: string;
+  color: string;
+  icon: IconDefinition;
+}
+
+const userLinks: Button[] = [
+  {
+    name: "Home",
+    color: "#000000",
+    icon: faHouse,
+  },
+  {
+    name: "Students",
+    color: "#000000",
+    icon: faUsers,
+  },
+  {
+    name: "Seniors",
+    color: "#000000",
+    icon: faUserGroup,
+  },
+  {
+    name: "Pending",
+    color: "#000000",
+    icon: faUserPlus,
+  },
+  {
+    name: "Profile",
+    color: "#000000",
+    icon: faCircleUser,
+  },
+];
+
+const SidebarItem = ({
+  label,
+  iconName,
+}: {
+  label: string;
+  iconName: IconDefinition;
+}) => {
   return (
-    <div className="w-full px-4 p-2 text-md text-left font-serif duration-150 hover:bg-darker-tan">
-      <div className="duration-150 hover:translate-x-1">{label}</div>
+    <div className="text-md flex w-full space-x-4 p-2 px-10 text-left font-serif duration-150 hover:translate-x-1">
+      <div className="w-1/6 duration-150 hover:translate-x-1">
+        <FontAwesomeIcon icon={iconName} />
+      </div>
+      <div className="">{label}</div>
     </div>
   );
 };
 
-const Sidebar = ({ displayName } : { displayName: string }) => {
+const Sidebar = () => {
   return (
-    <div className="flex flex-col w-[160px] h-screen top-0 py-12 justify-start gap-20 \
-                    bg-med-tan border-r border-darker-tan">
-          <div className="flex flex-col p-4 place-items-center">
-            <Link href="/">
-              <h1 className="font-serif text-center text-2xl duration-150 hover:-translate-y-0.5 z-10">
-                The Legacy Project
-              </h1>
-            </Link>
+    <div
+      className="\ top-0 flex h-screen w-[210px] flex-col justify-start border-r
+                    border-darker-tan bg-med-tan py-16"
+    >
+      <div className="flex flex-col place-items-center p-4">
+        <Link href="/">
+          <h1 className="z-10 text-center font-serif text-2xl duration-150 hover:-translate-y-0.5">
+            The Legacy Project
+          </h1>
+        </Link>
+      </div>
 
-            <span className="absolute h-[60px] aspect-square left-3 top-[48px] rotate-45 opacity-60">
-              <Image 
-                src="/landing/pink-flower.png"
-                layout="fill"
-                objectFit="cover"
-                // TODO(nickbar01234) - Write a more descriptive alt
-                alt="Pink flower"
-              />
-            </span>
-
-            <span className="absolute h-[60px] aspect-square top-[70px] left-[88px] opacity-40">
-              <Image 
-                src="/landing/yellow-flower.png"
-                layout="fill"
-                objectFit="cover"
-                // TODO(nickbar01234) - Write a more descriptive alt
-                alt="Yellow flower"
-              />
-            </span>
+      <div className="flex w-full flex-col">
+        {userLinks.map((data) => (
+          <div key={data.name}>
+            <SidebarItem label={data.name} iconName={data.icon} />
           </div>
+        ))}
+      </div>
 
-        <div className="flex flex-col w-full bg-dark-tan border border-darker-tan">
-          {/* TODO(nickbar01234) - Point this to something... */}
-          <Link href="/home">
-            <SidebarItem label="Home"/>
-          </Link>
-          <button onClick={() => signOut({ callbackUrl: "/" })}>
-            <SidebarItem label="Sign Out"/>
-          </button>
+      <div className="pt-15"></div>
+
+      <div className="text-md flex w-full px-10 pt-20 text-left font-serif text-xxs text-dark-gray">
+        <div className="w-4">
+          <FontAwesomeIcon icon={faCity} />
         </div>
+        University
+      </div>
+
+      <div className="text-md flex w-full px-10 pt-1 text-left font-serif text-xs">
+        Tufts University
+      </div>
+
+      <hr className="mx-10 my-10 h-px rounded border-0 bg-black md:my-10" />
+
+      <div className="text-md flex w-full px-10 text-left font-serif">
+        Kick Doyen
+      </div>
+      <div className="text-md flex w-full p-2 px-10 pb-10 pt-0 text-left font-serif text-xxs text-dark-gray">
+        Project/Tech Lead
+      </div>
+
+      <button onClick={() => signOut({ callbackUrl: "/" })}>
+        <SidebarItem label="Sign Out" iconName={faArrowRightFromBracket} />
+      </button>
     </div>
   );
 };
