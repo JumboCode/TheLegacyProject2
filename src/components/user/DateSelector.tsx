53f9d9f5660051b9452aa5bd6520caf267a2e916
nathan-j-edwards <nathanieljesseedwards@gmail.com>
2024-02-24 23:27:51 -0500

    added styling to calandar

--- src/components/user/DateSelector.tsx
+++ src/components/user/DateSelector.tsx
@@ -5,10 +5,12 @@ import "react-datepicker/dist/react-datepicker.css";
 const DateSelector = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <div className="text-black">
+    <div className="mb-4 font-['merriweather'] text-2xl text-[#22555A]">
       <DatePicker
+        className="h-[70px] w-[700px] rounded-lg pl-[30px]"
         selected={startDate}
         onChange={(date) => date && setStartDate(date)}
+        dateFormat="dd MMMM yyyy"
       />
     </div>
   );
